659388ab884e6fa9dfe0e88fdc17310b5e16b833
bagus18 <ciutirc@gmail.com>
2016-11-30 16:14:34 +0700

    vps

---
+++ vbalu.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get install oidentd
+apt-get install build-essential
+apt-get install libssl-dev
+apt-get install g++
+apt-get install ca-certificates
+apt-get install tcl-dev
+apt-get install dnsutils
+apt-get install cron
+apt-get install nano
